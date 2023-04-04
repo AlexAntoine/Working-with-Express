f2b1ff875a5bc32d08fde2b679addf4628da84db
Alex Antoine <antoinealex1234@gmail.com>
2023-04-04 16:20:28 -0400

    working with ejs

--- app.js
+++ app.js
@@ -5,4 +5,20 @@ const app = express();
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
+});
+
+app.get('/',(req, res)=>{
+    res.render('index.ejs')
+});
+
+app.get('/demo',(req, res)=>{
+    res.render('demo.ejs')
+});
+
+app.get('/user/:id/:username',(req, res)=>{
+    const id = req.params.id;
+    const username = req.params.username;
+
+    res.render('index.ejs', {id, username})
 })
+

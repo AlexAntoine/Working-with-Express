34c9a24cd9e1f6ccb77966b83b3be3ba252fa369
Alex Antoine <antoinealex1234@gmail.com>
2023-04-06 15:34:23 -0400

    completed movie search

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+require('dotenv');
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
 
 const port =  process.env.PORT|| 3000;
 const app = express();
@@ -10,19 +12,21 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
-
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
 
-app.get('/search',(req, res)=>{
+app.get('/',async(req, res)=>{
 
-    res.render('search')
+    res.render('search');
 });
 
-app.get('/results',(req, res)=>{
+app.get('/results',async(req, res)=>{
+ 
+    const {data} = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&query=${req.query.search}`);
 
-    res.render('movies');
+    res.render('movies', {data,searchQuery: req.query.search})
+
 })
 
 app.get('*',(req, res)=>{res.send('404. This page does not exist')})
